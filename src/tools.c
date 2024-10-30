1c634e14f90dcc3d4555b32ac20bceb89187cdbc
DaXor-0 <saverio.pasqualoni@yahoo.it>
2024-10-30 10:42:19 +0100

    Automated commit

---
+++ src/tools.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "tools.h"
+
+int set_initial_conditions(planet **target, int n_of_bodies){
+  unsigned int seed = time(NULL);
+  
+  for (int body = 0; body < n_of_bodies; body++){
+    target[body]->mass = (int)rand_r(&seed) / RAND_MAX * 100;
+    //target[body]->radius = (double)rand_r(&seed) / RAND_MAX * 100.0;
+    target[body]->pos[0] = (double)rand_r(&seed) / RAND_MAX * 100.0;
+    target[body]->pos[1] = (double)rand_r(&seed) / RAND_MAX * 100.0;
+    target[body]->vel[0] = (double)rand_r(&seed) / RAND_MAX * 100.0;
+    target[body]->vel[1] = (double)rand_r(&seed) / RAND_MAX * 100.0;
+  }
+
+  return 0;  // Success
+}
+
+
+
---
+++ src/tools.h
@@ -0,0 +1,17 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+#include <stdlib.h>
+
+typedef struct{
+  int mass;
+  //double radius;
+  double pos[2];
+  double vel[2];
+} planet;
+
+// 10.24 10.40
+
+int set_initial_conditions(planet **target, int n_of_bodies);
+
+#endif
