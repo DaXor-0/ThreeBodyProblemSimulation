17de330a4334cd5ac5c1f6319f436df086ea0d9e
DaXor-0 <saverio.pasqualoni@yahoo.it>
2024-11-02 22:24:22 +0100

    Automated commit

--- submit.sh
+++ submit.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
-# #SBATCH -N 7
-# #SBATCH -n 28
-# #SBATCH -p debug
-# # #SBATCH --gres=gpu:1
-# # #SBATCH --exclusive
+#SBATCH --nodes=7                  # Total number of nodes
+#SBATCH --ntasks-per-node=7        # Number of tasks per node
+#SBATCH --cpus-per-task=4          # Number of OpenMP threads per MPI task
+#SBATCH -p debug
+#SBATCH --exclusive
 # # #SBATCH --time=00:05:00
 # # #SBATCH --account=tra24_epicure
 
+RUN=srun
 
 N_OF_BODIES=(4 16 64 256 1024 4096 16384)
 GRID_SIZE=(200 400 800 1600 3200 6400 12800)
 
-RUN=mpicc
-N_PROC=2
-#RUN=srun
-
-index=1
-
-iter=20000
 TIMESTAMP=$(date +"%Y_%m_%d___%H:%M:%S")
 filename="./$TIMESTAMP.csv"
 gifname="./$TIMESTAMP.gif"
 
-$RUN -n $N_PROC ~/ThreeBodyProblemSimulation/simulation.out ${N_OF_BODIES[$index]} $iter $filename
+index=1
+iter=20000
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK 
+$RUN -n $SLURM_NTASKS ~/ThreeBodyProblemSimulation/simulation.out ${N_OF_BODIES[$index]} $iter $filename
 
 source ~/venv1/bin/activate
-python ~/ThreeBodyProblemSimulation/plot.py $filename $gifname ${N_OF_BODIES[$index]}
+python ~/ThreeBodyProblemSimulation/plot.py $filename $gifname ${GRID_SIZE[$index]}
