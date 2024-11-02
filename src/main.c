bebd8da2b03c3412bdf64e7cb077a9cc325e46e7
DaXor-0 <saverio.pasqualoni@yahoo.it>
2024-11-02 16:40:04 +0100

    Automated commit

--- src/main.c
+++ src/main.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]){
     delta_t = compute_new_delta_t(system_status.data, n_of_bodies);
     elapsed_time += delta_t;
       
-    // accumulate data every STORE_VAR iterations
+    // accumulate data every PRINT_INTERVAL time elapsed
     if(elapsed_time >  print_iter * PRINT_INTERVAL) { 
       accumulate_data(&store_buffer, print_iter % TMP_BUF_SIZE, n_of_bodies, &system_status);
       print_iter++;
@@ -49,23 +49,17 @@ int main(int argc, char *argv[]){
 
     time_step_update(system_status.data, n_of_bodies, delta_t);
   }
-<<<<<<< HEAD
 
-  printf("%.3f %.3f\n", delta_t, elapsed_time);
+  printf("\nLast delta_t is: %.5f\n TOTAL ELAPSED TIME:%.3f\n", delta_t, elapsed_time);
+  
   free_store_buffer(&store_buffer);
-=======
-  
-  if(rank == 0) printf("\nLast delta_t is: %.5f\n TOTAL ELAPSED TIME:%.3f\n", delta_t, elapsed_time);
-  
-  if (rank == 0) free_store_buffer(&store_buffer);
->>>>>>> main
   free(system_status.mass);
   free(system_status.data);
 
   return 0;
 
-  cleanup:
-    if ( NULL != store_buffer.mass || NULL != store_buffer.data )
+cleanup:
+  if ( NULL != store_buffer.mass || NULL != store_buffer.data )
     free_store_buffer(&store_buffer);
   if ( NULL != system_status.mass) free(system_status.mass);
   if ( NULL != system_status.data) free(system_status.data);
